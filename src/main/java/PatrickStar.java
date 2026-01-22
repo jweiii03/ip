1d8dff7c35e748c0ce5a6b4bef978fe90d8509ff
Jwei <hjianwei1@gmail.com>
2026-01-22 21:51:15 +0900

    Level-1: Add Echo feature

--- src/main/java/PatrickStar.java
+++ src/main/java/PatrickStar.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class PatrickStar {
     public static void main(String[] args) {
         String logo = "######                                                 \n"
@@ -9,7 +11,18 @@ public class PatrickStar {
                     + "#       #   #    #    #    #  #   ####  #    #    #    \n";
 
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hi, I'm Patrick!");
-        System.out.println("Is mayonnaise an instrument? Bye Bye!");
+        System.out.println("Hi, I'm Patrick star.");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        // Reads input until user types bye
+        while (!input.toLowerCase().equals("bye")) {
+            System.out.println("Uhhh...uhh..." + input);  // Echo user's input
+            input = scanner.nextLine();  // Read next inputs
+        }
+
+        System.out.println("Uhhh... Is mayonnaise an instrument? Bye bye.");
+        scanner.close();
     }
 }
