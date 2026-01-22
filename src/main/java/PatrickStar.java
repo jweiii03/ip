e3b18cfded0862eed14ab811a66e05617c858086
Jwei <hjianwei1@gmail.com>
2026-01-22 22:22:24 +0900

    Add Marking task feature

--- src/main/java/PatrickStar.java
+++ src/main/java/PatrickStar.java
@@ -13,8 +13,9 @@ public class PatrickStar {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hi, I'm Patrick star.");
 
-        String[] tasks = new String[100];
+        String[] tasks = new String[100]; // Store user's task
         int taskCount = 0;  // Track number of tasks
+        boolean[] isDone = new boolean[100];  // Track done status of tasks
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
@@ -25,8 +26,15 @@ public class PatrickStar {
                 System.out.println("Uhh... here are your tasks: ");
                 // Display all tasks added by users
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String status = isDone[i] ? "[X]" : "[ ]";
+                    System.out.println((i + 1) + "." + status + " " + tasks[i]);
                 }
+            } else if (input.toLowerCase().startsWith("mark ")) {
+                // Mark task as done
+                int taskNum = Integer.parseInt(input.substring(5)) - 1;
+                isDone[taskNum] = true;
+                System.out.println("Alright, yeah. I've marked this task as done:");
+                System.out.println("[X] " + tasks[taskNum]);
             } else {
                 // Add task
                 tasks[taskCount] = input;
