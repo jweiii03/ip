fa0c539836558bf038d9ba07bd2e8e191089f9b5
Jwei <hjianwei1@gmail.com>
2026-01-22 22:11:53 +0900

    Level-2: Added List feature

--- src/main/java/PatrickStar.java
+++ src/main/java/PatrickStar.java
@@ -13,13 +13,27 @@ public class PatrickStar {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hi, I'm Patrick star.");
 
+        String[] tasks = new String[100];
+        int taskCount = 0;  // Track number of tasks
+
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
         // Reads input until user types bye
         while (!input.toLowerCase().equals("bye")) {
-            System.out.println("Uhhh...uhh..." + input);  // Echo user's input
-            input = scanner.nextLine();  // Read next inputs
+            if (input.toLowerCase().equals("list")) {
+                System.out.println("Uhh... here are your tasks: ");
+                // Display all tasks added by users
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                // Add task
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("Alright, yeah. I added: " + input);
+            }
+            input = scanner.nextLine();
         }
 
         System.out.println("Uhhh... Is mayonnaise an instrument? Bye bye.");
