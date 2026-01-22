420e1119629ed4660524734266d2890023847e3f
Jwei <hjianwei1@gmail.com>
2026-01-22 22:51:49 +0900

    Created new class Deadline.java and Event.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,14 @@
+public class Deadline extends Task {
+    // Child class of Task parent class
+    private String dueDate;
+
+    public Deadline(String description, String dueDate) {
+        super(description);
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + dueDate + ")";
+    }
+}
