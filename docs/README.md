d313ceabd9200ae5124d114be490466ec4340331
Jwei <hjianwei1@gmail.com>
2026-01-22 21:34:34 +0900

    Renamed to PatrickStar.java

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# BeeJolly User Guide
+# PatrickStar User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/PatrickStar.java
@@ -0,0 +1,17 @@
+public class PatrickStar {
+    public static void main(String[] args) {
+        String logo = "######                                                 \n"
+                    + "#     #  ###   #####  #####   #   ####  #    #    #    \n"
+                    + "#     # #   #    #    #    #     #    # #   #     #    \n"
+                    + "######  #   #    #    #    #  #  #      ####      #    \n"
+                    + "#       #####    #    #####   #  #      #  #      #    \n"
+                    + "#       #   #    #    #   #   #  #    # #   #          \n"
+                    + "#       #   #    #    #    #  #   ####  #    #    #    \n";
+
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hi, I'm Patrick!");
+        System.out.println("Is mayonnaise an instrument? Bye Bye!");
+
+
+    }
+}
