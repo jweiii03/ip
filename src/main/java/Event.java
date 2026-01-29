41d80fc51145c7704be761e6aabedf46c07ac4b7
Jwei <hjianwei1@gmail.com>
2026-01-29 12:51:08 +0800

    Level-8: Add date and time feature

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -22,7 +22,7 @@ public class Event extends Task {
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy HHmm");
-        return "[E]" + super.toString() + " (from: " + from.format(formatter) + " to: " + to.format(formatter) + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(formatter) + ", to: " + to.format(formatter) + ")";
     }
 
     @Override
